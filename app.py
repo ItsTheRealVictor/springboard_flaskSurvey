f1e3fa01141b43061e3413c836cf6cb4518c39d0
Victor Delgado <valexdelgado@gmail.com>
2022-11-10 09:07:51 -0800

    added jinja templates with base.html

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import Question, Survey, personality_quiz, satisfaction_survey, surveys
+from surveys import Question, Survey, satisfaction_survey, surveys
 
 app = Flask(__name__)
 
